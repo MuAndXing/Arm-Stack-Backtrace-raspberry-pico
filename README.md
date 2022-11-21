2080b989e85f414096eb7e3d191e62c7958f9623
qiyang.xing <snown@live.cn>
2022-11-21 13:18:36 +0800

    delete the redundant stack printing information and just print out the address of the calling function

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 《Cortex-M0+ Technical Reference Manual》  
 《ARM® Compiler armasm User Guide》  
 《RP2040 Datasheet A microcontroller by Raspberry Pi》  
-![image](https://github.com/xqy828/Raspberry-PICO-Stack-Backtrace/blob/main/images/2022-11-20_211746.png)
+![image](https://github.com/xqy828/Raspberry-PICO-Stack-Backtrace/blob/main/images/2022-11-21_131406.png)
--- main.c
+++ main.c
@@ -8,29 +8,54 @@
 #include <stdio.h>
 #include "pico/version.h"
 
-typedef unsigned int uint;
+ 
+#define RAM_STACK_BOTTOM_ADDR   (0x20042000u)
+#define RAM_STACK_SIZE          (0x800u)
+
+#define text_section_start(_type)\
+({\
+	static char start[0] __aligned(4)\
+	__attribute__((unused)) \
+    __section(".text");\
+	(_type)&start;\
+})
+
+
+extern unsigned int __text_start__;
+extern unsigned int __text_end__;
 
 void HardFault_Handler(unsigned int * hardfault_args,unsigned int lr_value)
 {
-    
+#define BL_INS_MASK     (0xF800u)
+#define BL_INS_HIGH     (0xF800u)
+#define BL_INS_LOW      (0xF000u)
+#define BLX_INS_MASK    (0xFF00u)
+#define BLX_INX         (0x4700u)
+
     unsigned int *stack_addr = NULL;
     int i = 0;
-    uint stack_r0;
-    uint stack_r1;
-    uint stack_r2;
-    uint stack_r3;
-    uint stack_r4;
-    uint stack_r5;
-    uint stack_r6;
-    uint stack_r7;
-    uint stack_r8;
-    uint stack_r9;
-    uint stack_r10;
-    uint stack_r11;
-    uint stack_r12;
-    uint stack_lr;
-    uint stack_pc;
-    uint stack_psr;
+    unsigned int text_start = 0;
+    unsigned int text_end = 0;
+    unsigned int stack_value = 0;
+    unsigned short insh = 0;
+    unsigned short insl = 0;
+    unsigned int print_cnt = 0;
+    unsigned int stack_r0 = 0;
+    unsigned int stack_r1 = 0;
+    unsigned int stack_r2 = 0;
+    unsigned int stack_r3 = 0;
+    unsigned int stack_r4 = 0;
+    unsigned int stack_r5 = 0;
+    unsigned int stack_r6 = 0;
+    unsigned int stack_r7 = 0;
+    unsigned int stack_r8 = 0;
+    unsigned int stack_r9 = 0;
+    unsigned int stack_r10 = 0;
+    unsigned int stack_r11 = 0;
+    unsigned int stack_r12 = 0;
+    unsigned int stack_lr = 0;
+    unsigned int stack_pc = 0;
+    unsigned int stack_psr = 0;
     
     stack_r0 = (unsigned int)*hardfault_args;
     stack_r1 = (unsigned int)*(hardfault_args+1);
@@ -41,7 +66,8 @@ void HardFault_Handler(unsigned int * hardfault_args,unsigned int lr_value)
     stack_pc = (unsigned int)*(hardfault_args+6);
     stack_psr = (unsigned int)*(hardfault_args+7);
 
-
+    text_start = (unsigned int)&__text_start__;
+    text_end = (unsigned int)&__text_end__;
 
     printf("stack psr: 0x%08x\r\n",stack_psr);
     printf("stack r0: 0x%08x\r\n", stack_r0);
@@ -62,22 +88,47 @@ void HardFault_Handler(unsigned int * hardfault_args,unsigned int lr_value)
     printf("current lr: 0x%08x\r\n",lr_value);
     printf("FaultHandler\n");
     //asm("bkpt");
+    printf(".text section range:0x%08x-0x%08x .\r\n",text_start,text_end);
     stack_addr = hardfault_args;
     for(i = 1;i < 512; i++)
     {   
-        if((unsigned int)stack_addr == 0x20042000)// from .ld in pico sdk 
+        if((unsigned int)stack_addr == RAM_STACK_BOTTOM_ADDR)
         {
-            printf("\r\nis stack bottom, break !\r\n");
+            printf("\r\nstack bottom, break !\r\n");
             break;
         }
-        printf("0x%08x-0x%08x|",stack_addr,*stack_addr);
-        stack_addr++;
-        if(i%4 == 0)
+        // thumb mode
+        if(*stack_addr & 0x1)
         {
-            printf("\r\n");
+            // check the addr value if is in .text section range 
+            if((*stack_addr >= text_start) && (*stack_addr < text_end))
+            {
+                stack_value = (*stack_addr & 0xFFFFFFFE) - 4;
+                //detect if thumb BL or BLX machine code 
+                insl = *(unsigned short*)stack_value;
+                insh = *(unsigned short*)(stack_value+2);
+                if(((insh & BL_INS_MASK) == BL_INS_HIGH) &&
+                    ((insl & BL_INS_MASK) == BL_INS_LOW)
+                  )
+                {
+                    printf("0x%08x-0x%08x|",stack_addr,stack_value);
+                    print_cnt++;
+                }
+                else if((insh & BLX_INS_MASK) == BLX_INX )
+                {
+                    printf("0x%08x-0x%08x|",stack_addr,stack_value);
+                    print_cnt++;
+                }
+                if(print_cnt%4 == 0)
+                {
+                    printf("\r\n");
+                }
+            }
         }
+        stack_addr++;
     }
     printf("\r\n");
+    gpio_put(PICO_DEFAULT_LED_PIN, 1);
     for(;;);
 }
 
